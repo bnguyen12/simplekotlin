7e5db5fea7a9640d7884ddcb5b58313d0c3c14b9
Benny Nguyen <benny.nguyenx@gmail.com>
2018-04-04 19:19:57 -0700

    Finish assignment to pass all tests

--- main.kts
+++ main.kts
@@ -3,7 +3,6 @@
 println("UW Homework: Simple Kotlin")
 
 // write a "whenFn" that takes an arg of type "Any" and returns a String
-
 fun whenFn(word: Any): String {
     when(word) {
         0 -> return("zero")
@@ -17,30 +16,79 @@ fun whenFn(word: Any): String {
 }
 
 // write an "add" function that takes two Ints, returns an Int, and adds the values
-
 fun add(left: Int, right: Int): Int {
-    return left + right
+    return(left + right)
 }
 
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
-
 fun sub(left: Int, right: Int): Int {
-    return left - right
+    return(left - right)
 }
-// write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
 
+// write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
 fun mathOp(left: Int, right: Int, op: (Int, Int) -> Int): Int {
-    return op(left, right)
+    return(op(left, right))
 }
 
 // write a class "Person" with first name, last name and age
-
 class Person(val firstName: String, val lastName: String, var age: Int) {
-    
+    val debugString: String 
+        get() = "[Person firstName:${firstName} lastName:${lastName} age:${age}]" 
+        
+    fun hashCode(): Int {
+        return(firstName.hashCode() * lastName.hashCode() + age)
+    }
+
+    fun equals(other: Person): Boolean {
+        return(this.hashCode() == other.hashCode())
+    }
 }
 
 // write a class "Money"
+class Money(var amount: Int, var currency: String) {
+    init {
+        if (currency != "USD" && currency != "EUR" && 
+            currency != "CAN" && currency != "GBP") {
+            throw Exception("Currency not supported!")
+        } else if (amount < 0) {
+            throw Exception("Amount can't be less than zero!")
+        }
+    }
+ 
+    fun convert(newCurrency: String): Money {
+        var newAmount = 10 //default value for 10USD
+        when (this.currency) {
+            "USD" -> when (newCurrency) {
+                "GBP" -> newAmount = 5
+                "USD" -> newAmount = 10
+                "EUR" -> newAmount = 15
+                "CAN" -> newAmount = 15
+            }
 
+            "GBP" -> when (newCurrency) {
+                "GBP" -> newAmount = 5
+                "USD" -> newAmount = 10
+                "EUR" -> newAmount = 15
+            }
+
+            "EUR" -> when (newCurrency) {
+                "GBP" -> newAmount = 5
+                "USD" -> newAmount = 10
+                "EUR" -> newAmount = 15
+            }
+
+            "CAN" -> when(newCurrency) { 
+                "USD" -> newAmount = 12
+            }
+        }
+        return Money(newAmount, newCurrency)
+    }
+
+    operator fun plus(other: Money): Money {
+        var changeMoney = other.convert(this.currency)
+        return(Money(changeMoney.amount + this.amount, this.currency))
+    }
+}
 // ============ DO NOT EDIT BELOW THIS LINE =============
 
 print("When tests: ")
